2edaaaff5d55018a861f2353eb3d31bb56e80074
setogen <springh230330kz@gmail.com>
2017-01-23 15:01:56 +0900

    2017/01/23

--- sf2toxi.py
+++ sf2toxi.py
@@ -8,6 +8,7 @@ The program purpose is soundfont2 export to xi(Fasttracker2)
 import os
 import sys
 import re
+import codecs
 from struct import * 
 
 #------------sf2 variables------------------------------------------------------
@@ -31,39 +32,39 @@ bit_per_second = 0
 #---------------------xi variables-----------------------------------------------
 extxi="Extended Instrument: " #x00
 name=""   # Name,0x1A :x15
-trkname="FastTracker v2.00"# Tracker name,f.e. "FastTracker v2.00" in FT2's case :x2c
-major_version=2             # :x40
-minor_version=1 #:x41
-samples_note_map=[0 for i in range(96)]#'0'*96 :x42
-volume_envelope=[0 for i in range(24)]#[2]*24 :xA2
-pannning_envelope=[0 for i in range(24)]#[2]*24:xD2
-ppvolume_points_number=0#[1]:x102
-pannning_points_number=0#   :x103
-volume_sustain_point=0#   :x104
-volume_loop_start_point=0# :x105
-volume_loop_end_point=0#    :x106
-panning_sustain_point=0#    :x107
-panning_loop_start_point=0# :x108
-panning_loop_end_point=0#:x109
-volume_type=0#           :x10A
-panning_type=0#          :x10B
-vibrato_type=0#          :x10C
-vibrato_sweep =0#        :x10D
-vibrato_depth=0#         :x10E
-vibrato_rate=0#          :x10F
-volume_fadeout=0# :x110
-extended_info=0#  :x112
-samples_number=0#:x128
-sample_length=0#:x12A
-sample_loop_start=0#:x12E
-sample_loop_length=0#:x132
-sample_volume=100#:x136
-sample_finetune=0#:x137
-sample_type=0#:x138
-sample_panning=0#:x139
-sample_transpose=0#:x13A
-sample_sample_name_length=0#:x13B
-sample_name='sample001'#:x13C
+trkname="FastTracker v2.00   "# Tracker name,f.e. "FastTracker v2.00" in FT2's case :x2c
+major_version=bytes('\x02')             # :x40
+minor_version=bytes('\x01') #:x41
+samples_note_map=bytes('\x00'*96)#'0'*96 :x42
+volume_envelope=bytes('\x00\x00\x40\x00\x20\x00\x40\x00')+bytes('\x00'*40)#[2]*24 :xA2
+panning_envelope=bytes('\x00\x00\x20\x00\x20\x00\x20\x00')+bytes('\x00'*40) #[2]*24:xD2
+volume_points_number=bytes('\x02')#[1]:x102
+panning_points_number=bytes('\x02')#   :x103
+volume_sustain_point=bytes('\x00')#   :x104
+volume_loop_start_point=bytes('\x00')# :x105
+volume_loop_end_point=bytes('\x01')#    :x106
+panning_sustain_point=bytes('\x00')#    :x107
+panning_loop_start_point=bytes('\x00')# :x108
+panning_loop_end_point=bytes('\x01')#:x109
+volume_type=bytes('\x00')#           :x10A
+panning_type=bytes('\x00')#          :x10B
+vibrato_type=bytes('\x00')#          :x10C
+vibrato_sweep =bytes('\x00')#        :x10D
+vibrato_depth=bytes('\x00')#         :x10E
+vibrato_rate=bytes('\x00')#          :x10F
+volume_fadeout=bytes('\xff\x7f')# :x110
+extended_info=bytes('\x00'*22)#  :x112
+samples_number=bytes('\x01\x00')#:x128
+sample_length=''#:x12A
+sample_loop_start=''#:x12E
+sample_loop_length=''#:x132
+sample_volume=bytes('\x40')#:x136
+sample_finetune=bytes('\xe4')#:x137
+sample_type=bytes('\x01')#:x138
+sample_panning=bytes('\x80')#:x139
+sample_transpose=bytes('\x11')#:x13A
+sample_name_length=bytes('\x00')#:x13B
+sample_name=bytes('\x00'*22)#:x13C
 
 '''xi file structure
 CHAR extxi[21];   //"Extended Instrument :x00
@@ -168,11 +169,53 @@ smpl_start = matchOB.end()+4
 smpldata = data[smpl_start:smpl_start+smpldata_size]
 for i in range(repeat):
     shdr_list[i].append(smpldata[shdr_list[i][1]:shdr_list[i][2]])
-'''
+
 for i in shdr_list:
-    print(i[0])
-'''
+    print(i[0:-1])
+
 try:
+        print('Making directories')
 	os.mkdir(dir_name)
 except(OSError):
-	print('error has occured')
+	print('Directorie already exists')
+os.chdir(dir_name)
+for i in shdr_list:
+    xi_file = open(i[0].strip('\x00')+'.xi','wb')
+    xi_file.write(extxi)
+    xi_file.write(i[0]) #write sample's name
+    xi_file.write(bytes('\x00\x00\x1A')) #extends sample's name
+    xi_file.write(trkname)
+    xi_file.write(major_version)
+    xi_file.write(minor_version)
+    xi_file.write(samples_note_map)
+    xi_file.write(volume_envelope)
+    xi_file.write(panning_envelope)
+    xi_file.write(volume_points_number)
+    xi_file.write(panning_points_number)
+    xi_file.write(volume_sustain_point)
+    xi_file.write(volume_loop_start_point)
+    xi_file.write(volume_loop_end_point)
+    xi_file.write(panning_sustain_point)
+    xi_file.write(panning_loop_start_point)
+    xi_file.write(panning_loop_end_point)
+    xi_file.write(volume_type)
+    xi_file.write(panning_type)
+    xi_file.write(vibrato_type)
+    xi_file.write(vibrato_sweep)
+    xi_file.write(vibrato_depth)
+    xi_file.write(vibrato_rate)
+    xi_file.write(volume_fadeout)
+    xi_file.write(extended_info)
+    xi_file.write(samples_number)
+    xi_file.write(pack('<L',i[2]-i[1])) #writing sample length(dwend-dwstart)
+    xi_file.write(pack('<L',i[3])) #writing sample loop start 
+    xi_file.write(pack('<L',i[4]-i[3])) #writing sample loop length(dwend_loop-dwstart_loop)
+    xi_file.write(sample_volume)
+    xi_file.write(sample_finetune)
+    xi_file.write(sample_type)
+    xi_file.write(sample_panning)
+    xi_file.write(sample_transpose)
+    xi_file.write(sample_name_length)
+    xi_file.write(sample_name)
+    xi_file.write(i[-1]) #writing sample data
+    xi_file.close()
